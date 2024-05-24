75fd2192bed8c75e5a9ecfcc6630257f947bdddc
Ahmed Ashraf <ahmedabdou1410@gmail.com>
2024-05-24 13:30:34 +0300

    Add .env.example and settings file

---
+++ app/settings.py
@@ -0,0 +1,9 @@
+import os
+
+DB_USER: str = os.environ.get("db_user") or ""
+DB_PORT: int = int(os.environ.get("db_port") or 5432)
+DB_NAME: str = os.environ.get("db_name") or ""
+DB_HOST: str = os.environ.get("db_host") or ""
+DB_PASSWORD: str = os.environ.get("db_password") or ""
+
+OPENAPI_KEY: str = os.environ.get("openapi_key") or ""
