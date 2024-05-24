4a646e630bc7121ad9e5664d47a72370cac1537e
Ahmed Ashraf <ahmedabdou1410@gmail.com>
2024-05-24 15:44:01 +0300

    Add supabase db connection pooler

---
+++ app/db/session.py
@@ -0,0 +1,21 @@
+"""
+Sets up postgresql database connection pool.
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import sessionmaker, Session
+import settings
+
+engine = create_engine(
+    f"postgresql+psycopg2://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}",
+    future=True,
+    pool_pre_ping=True,
+    pool_size=5,
+    pool_timeout=100,
+)
+
+
+session_maker = sessionmaker(engine)
+
+
+def get_db() -> Session:
+    return session_maker()
